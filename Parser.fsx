21e26b45aaf6618e43a1171c5e52c56192a7806a
Mark Shevchenko <markshevchenko@gmail.com>
2023-05-02 11:20:34 +0300

    Подкорректировал парсеры

--- Parser.fsx
+++ Parser.fsx
@@ -13,7 +13,7 @@ isChar 'c' ['c'; '1'; '2']
 "c12" |> List.ofSeq |> isChar 'a'
 "" |> List.ofSeq |> isChar 'c'
 
-let rec repeated parser chars =
+let rec internal repeated parser chars =
     match parser chars with
     | Some result, cs -> let results, cs = repeated parser cs 
                          result::results, cs
@@ -35,32 +35,32 @@ let map mapper parser chars =
     let result, cs = parser chars
     Option.map mapper result, cs
     
-let digits = whereChar Char.IsDigit
-          |> repeated1
-          |> map (String.concat "")
+let digits1 = whereChar Char.IsDigit
+           |> repeated1
+           |> map (String.concat "")
 
-"321" |> List.ofSeq |> digits
+"321" |> List.ofSeq |> digits1
 
-let (.>.) parser1 parser2 combinator chars =
+let (.>>.) parser1 parser2 combinator chars =
     match parser1 chars with
     | Some s1, cs1 -> match parser2 cs1 with
                       | Some s2, cs2 -> Some (combinator s1  s2), cs2
                       | _ -> None, chars
     | _ -> None, chars
 
-"a3" |> List.ofSeq |> (whereChar Char.IsLetter .>. whereChar Char.IsDigit) (+)
+"a3" |> List.ofSeq |> (whereChar Char.IsLetter .>>. whereChar Char.IsDigit) (+)
 
 let optional defaultValue parser chars =
     match parser chars with
     | Some s, cs -> Some s, cs
     | None, cs -> Some defaultValue, cs
 
-"a" |> List.ofSeq |> optional "" (whereChar Char.IsLetter)
-"1" |> List.ofSeq |> optional "" (whereChar Char.IsLetter)
+"a" |> List.ofSeq |> optional "NONE" (whereChar Char.IsLetter)
+"1" |> List.ofSeq |> optional "NONE" (whereChar Char.IsLetter)
 
-let fractional_part = (isChar '.' .>. digits) (+)
+let fractional_part = (isChar '.' .>>. digits1) (+)
 
-let number = (digits .>. optional "" fractional_part) (+)
+let number = (digits1 .>>. optional "" fractional_part) (+)
           |> map (fun s -> Convert.ToDouble(s, CultureInfo.InvariantCulture))
           
 "321" |> List.ofSeq |> number
@@ -85,19 +85,23 @@ let (<|>) parser1 parser2 chars =
     | Some result, cs -> Some result, cs
     | None, cs -> parser2 cs
     
-let rec value chars = (number
-                  <|> (isChar '(' >>. expression .>> isChar ')')
-                  <|> (isChar 's' >>. isChar 'i' >>. isChar 'n' >>. value |> map sin)) chars
-and star = isChar '*' >>. value
-and slash = isChar '/' >>. value |> map ((/)1.0)
-and term = (value .>. repeated0 (star <|> slash)) (fun v vs -> v * List.fold (*) 1.0 vs)
-and plus = isChar '+' >>. term
-and minus = isChar '-' >>. term |> map (~-)
-and expression = (term .>. repeated0 (plus <|> minus)) (fun v vs -> v + List.sum vs)
+let spaces0 = repeated0 (isChar ' ')
+let spaces1 = repeated1 (isChar ' ')
+
+let rec value chars = (number .>> spaces0
+                  <|> (isChar '(' >>. spaces0 >>. expression .>> isChar ')' .>> spaces0)
+                  <|> (isChar 's' >>. isChar 'i' >>. isChar 'n' >>. spaces1
+                       >>. value .>> spaces0 |> map sin)) chars
+and star = isChar '*' >>. spaces0 >>. value .>> spaces0
+and slash = isChar '/' >>. spaces0 >>. value .>> spaces0 |> map ((/)1.0)
+and term = (value .>> spaces0 .>>. repeated0 (star <|> slash)) (List.fold (*))
+and plus = isChar '+' >>. spaces0 >>. term .>> spaces0
+and minus = isChar '-' >>. spaces0 >>. term .>> spaces0 |> map (~-)
+and expression = (term .>> spaces0 .>>. repeated0 (plus <|> minus)) (List.fold (+))
 
 //
 
-"1*2*3/4" |> List.ofSeq |> expression
-"1*2+3*4" |> List.ofSeq |> expression
-"1*(2+3)*4" |> List.ofSeq |> expression
-"sin(3.1415/6)" |> List.ofSeq |> expression
+"1 * 2 * 3/4" |> List.ofSeq |> expression
+"1 * 2 + 3 * 4" |> List.ofSeq |> expression
+"1 * (2 + 3) * 4" |> List.ofSeq |> expression
+"sin (3.1415 / 6)" |> List.ofSeq |> expression
